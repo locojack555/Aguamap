eba3f4f9334ee15949e67e84092a23551fc460d9
adria <agonzalez1@alumnat.copernic.cat>
2026-02-20 18:01:09 +0100

    Desarrollo categorías
    Desarrollo de la pantalla, las data classes, los viewmodel y las screen de categorías. Modificación del navigationGraph para que la pant. categoría se enlace con el icono del menú

---
+++ app/src/main/java/cat/copernic/aguamap1/domain/model/Category.kt
@@ -0,0 +1,9 @@
+package cat.copernic.aguamap1.domain.model
+
+data class Category(
+    val id: String = "",
+    val name: String = "",
+    val icon: String = "💧",
+    val color: String = "#3B82F6",
+    val description: String = ""
+)
